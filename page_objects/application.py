1245bdfe40045a13fdd347f0bcf6c9a56a4f1961
MelnykQA <melnyk.qa@gmail.com>
2024-10-14 22:16:26 +0300

    Playwright Python: 23 Reporting

--- page_objects/application.py
+++ page_objects/application.py
@@ -1,4 +1,5 @@
 import logging
+import allure
 from playwright.sync_api import Browser
 from playwright.sync_api import Request, Route, ConsoleMessage, Dialog
 from .test_cases import TestCases
@@ -23,21 +24,25 @@ class App:
         self.page.on('console', console_handler)
         self.page.on('dialog', dialog_handler)
 
+    @allure.step
     def goto(self, endpoint: str, use_base_url=True):
         if use_base_url:
             self.page.goto(self.base_url + endpoint)
         else:
             self.page.goto(endpoint)
 
+    @allure.step
     def navigate_to(self, menu: str):
         self.page.click(f"css=header >> text=\"{menu}\"")
         self.page.wait_for_load_state()
 
+    @allure.step
     def login(self, login: str, password: str):
         self.page.fill("input[name=\"username\"]", login)
         self.page.fill("input[name=\"password\"]", password)
         self.page.press("input[name=\"password\"]", "Enter")
 
+    @allure.step
     def create_test(self, test_name: str, test_description: str):
         self.page.fill("input[name=\"name\"]", test_name)
         self.page.get_by_label("Test description").click()
@@ -45,58 +50,40 @@ class App:
         # self.page.fill("input[name=\"description\"]", test_description)
         self.page.click("input[type=\"submit\"]")
 
-
-    def check_test_exists(self, test_name: str):
-        return self.page.query_selector(f'css=tr >> text=\"{test_name}\"') is not None
-
-    #def delete_test_by_name(self, test_name: str):
-        #row = self.page.query_selector(f'*css=tr >> text=\"{test_name}\"')
-        #row.query_selector('button = \"{Delete}\"').click()
-        #self.page.get_by_role("row", name="{test_name}").get_by_role("button").nth(3).click()
-        #self.page.get_by_role("row", name=test_name).get_by_role("button").nth(3).click()
-
-    def delete_test_by_name(self, test_name: str):
-        # Знаходимо рядок, що містить назву тесту
-        row = self.page.get_by_role("row", name=test_name)
-
-        # Перевіряємо, чи знайдено рядок
-        if row:
-            # Знаходимо кнопку видалення всередині рядка і натискаємо її
-            delete_button = row.get_by_role("button").nth(3)
-            if delete_button:
-                delete_button.click()
-            else:
-                print("Кнопка видалення не знайдена.")
-        else:
-            print("Рядок з назвою тесту не знайдено.")
-
-
+    @allure.step
     def click_menu_button(self):
         self.page.wait_for_selector('.menuBtn')
         self.page.click('.menuBtn')
 
+    @allure.step
     def is_menu_button_visible(self):
         return self.page.is_visible('.menuBtn')
 
+    @allure.step
     def get_location(self):
         return self.page.text_content('.position')
 
+    @allure.step
     def intercept_requests(self, url: str, payload: str):
         def handler(route: Route, request: Request):
             route.fulfill(status=200, body=payload)
 
         self.page.route(url, handler)
 
+    @allure.step
     def stop_intercept(self, url: str):
         self.page.unroute(url)
 
+    @allure.step
     def refresh_dashboard(self):
         self.page.click('input')
         self.page.wait_for_event('response')
 
+    @allure.step
     def get_total_tests_stats(self):
         return self.page.text_content('.total >> span')
 
+    @allure.step
     def close(self):
         self.page.close()
         self.context.close()
--- page_objects/test_cases.py
+++ page_objects/test_cases.py
@@ -1,12 +1,15 @@
+import allure
 from playwright.sync_api import Page
 
 class TestCases:
     def __init__(self, page: Page):
         self.page = page
 
+    @allure.step
     def check_test_exists(self, test_name: str):
         return self.page.query_selector(f'css=tr >> text=\"{test_name}\"') is not None
 
+    @allure.step
     def delete_test_by_name(self, test_name: str):
         # Знаходимо рядок, що містить назву тесту
         row = self.page.get_by_role("row", name=test_name)
@@ -22,7 +25,7 @@ class TestCases:
             print("Рядок з назвою тесту не знайдено.")
         self.page.wait_for_timeout(300)
 
-
+    @allure.step
     def check_columns_hidden(self):
         description = self.page.is_hidden('.thDes')
         author = self.page.is_hidden('.thAuthor')
